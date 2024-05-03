2e7edc8cffab93c24cf814be6df733dd306d2497
unknown <samuel.lekpor@gmail.com>
2024-05-03 22:26:24 +0000

    Run the linters

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let hamburger = document.querySelector('.hamburger')
-let close = document.querySelector('.close')
-let navbar = document.querySelector('.navbar')
-let body = document.querySelector('body')
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const navbar = document.querySelector('.navbar');
+const body = document.querySelector('body');
 
 function toggleMenu() {
-    if (close.style.display === 'none') {
-        hamburger.style.display = 'none'
-        close.style.display = 'inline-block'
-				navbar.style.display = 'block'
-				body.classList.add('mobile-menu-active')
-    }else {
-        close.style.display = 'none'
-        hamburger.style.display = 'block'
-				navbar.style.display = 'none'
-				body.classList.remove('mobile-menu-active')
-    }
+  if (close.style.display === 'none') {
+    hamburger.style.display = 'none';
+    close.style.display = 'inline-block';
+    navbar.style.display = 'block';
+    body.classList.add('mobile-menu-active');
+  } else {
+    close.style.display = 'none';
+    hamburger.style.display = 'block';
+    navbar.style.display = 'none';
+    body.classList.remove('mobile-menu-active');
+  }
 }
 
-hamburger.addEventListener('click', toggleMenu)
-close.addEventListener('click', toggleMenu)
+hamburger.addEventListener('click', toggleMenu);
+close.addEventListener('click', toggleMenu);
